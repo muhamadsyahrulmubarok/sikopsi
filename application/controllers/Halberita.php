7909b50776dd7b6177fd844845154913d7b2ccde
Syahrul Mubarok <12172949@bsi.ac.id>
2020-05-01 22:21:13 +0700

    update 01/05

---
+++ application/controllers/Halberita.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Halberita extends CI_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('M_berita');
+    }
+
+    public function index()
+    {
+        $data = array(
+            'data' => $this->M_berita->GetAll()->result_array()
+        );
+        $this->load->view('frontend/v_berita/index', $data);
+    }
+}
--- application/views/backend/v_berita/tambah.php
+++ application/views/backend/v_berita/tambah.php
@@ -91,7 +91,7 @@
         <div class="container-fluid">
             <div class="row">
                 <div class="col-md-12 text-center">
-                    2018 - 2019 &copy; Greeva theme by <a href="">Coderthemes</a>
+                    2020 &copy; PRIMKOPPABRI USP 20 Slawi
                 </div>
             </div>
         </div>
