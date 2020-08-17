6302604c6f0a7ef728cac58196ef375ff8be2e76
Syahrul Mubarok <12172949@bsi.ac.id>
2020-08-17 16:00:12 +0700

    update

--- berita.sql
+++ berita.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 17 Agu 2020 pada 10.51
+-- Waktu pembuatan: 17 Agu 2020 pada 10.58
 -- Versi server: 10.4.8-MariaDB
 -- Versi PHP: 7.3.11
 
@@ -22,46 +22,12 @@ SET time_zone = "+00:00";
 -- Database: `db_sikopsi`
 --
 
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `berita`
---
-
-CREATE TABLE `berita` (
-  `id_berita` int(11) NOT NULL,
-  `judul_berita` varchar(256) NOT NULL,
-  `isi_berita` text NOT NULL,
-  `tgl_berita` date NOT NULL,
-  `img_berita` varchar(128) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
 --
 -- Dumping data untuk tabel `berita`
 --
 
 INSERT INTO `berita` (`id_berita`, `judul_berita`, `isi_berita`, `tgl_berita`, `img_berita`) VALUES
 (2, 'Jokowi Siapkan Rp 1 T Buat Bantu Koperasi', '<p><strong style=\"font-weight: bold; font-family: Helvetica, Helvetica-FF, Arial, Tahoma, sans-serif; font-size: 16px;\">Jakarta</strong><span style=\"font-family: Helvetica, Helvetica-FF, Arial, Tahoma, sans-serif; font-size: 16px;\">&nbsp;-&nbsp;</span></p><p style=\"margin-bottom: 16px; display: inline; font-family: Helvetica, Helvetica-FF, Arial, Tahoma, sans-serif; font-size: 16px;\">Presiden Joko Widodo (<a href=\"https://www.detik.com/tag/jokowi\" style=\"background-image: initial; background-position: initial; background-size: initial; background-repeat: initial; background-attachment: initial; background-origin: initial; background-clip: initial; color: var(--finance-blue-1); transition: all 0.3s ease-in-out 0s;\">Jokowi</a>) meminta Lembaga Pengelola Dana Bergulir Usaha Mikro Kecil dan Menengah (LPDB-UMKM) mengalirkan dana sebesar Rp 1 triliun kepada koperasi. Dana tersebut nantinya akan disalurkan kembali kepada pelaku UMKM dalam bentuk modal kerja.</p><p style=\"margin-top: 16px; margin-bottom: 16px; font-family: Helvetica, Helvetica-FF, Arial, Tahoma, sans-serif; font-size: 16px;\">Pemberian dana bergulir ini, dikatakan&nbsp;<a href=\"https://www.detik.com/tag/jokowi\" style=\"background-image: initial; background-position: initial; background-size: initial; background-repeat: initial; background-attachment: initial; background-origin: initial; background-clip: initial; color: var(--finance-blue-1); transition: all 0.3s ease-in-out 0s;\">Jokowi</a>&nbsp;masuk menjadi bagian percepatan pemulihan ekonomi nasional.</p><p style=\"margin-bottom: 16px; font-family: Helvetica, Helvetica-FF, Arial, Tahoma, sans-serif; font-size: 16px;\">\"Saya senang tadi bahwa disiapkan oleh LPDB Rp 1 triliun, betul pak dirut? Ini segera berikan kepada koperasi-koperasi yang baik agar dari koperasi juga diberikan kepada anggota-anggotanya, pelaku-pelaku usaha secepat-cepatnya,\" kata Jokowi di Istana Negara via virtual, Jakarta, Kamis (23/7/2020).</p><p style=\"margin-bottom: 16px; font-family: Helvetica, Helvetica-FF, Arial, Tahoma, sans-serif; font-size: 16px;\">Dari anggaran Rp 1 triliun, Jokowi menyebut yang sudah tersalurkan kepada koperasi sebesar Rp 381 miliar. Dia pun meminta penyaluran dana bergulir terus ditingkatkan dan jangan sampai terhenti.</p><p style=\"margin-bottom: 16px; font-family: Helvetica, Helvetica-FF, Arial, Tahoma, sans-serif; font-size: 16px;\">Menurut Jokowi, penyaluran dana bergulir ini memberikan peran penting dalam meningkatkan ekonomi nasional.</p><p style=\"margin-bottom: 16px; font-family: Helvetica, Helvetica-FF, Arial, Tahoma, sans-serif; font-size: 16px;\">\"Ini kesempatan kita ada di sini dan bantuan modal kerja ini akan memperbaiki likuiditas dari koperasi. Tadi disampaikan pak menteri koperasi nilainya dari Rp 2 miliar sampai Rp 50 miliar. Ini alhamdulillah bunganya tadi juga disampaikan 3% menurun, ini juga sangat kompetitif sekali,\" ujarnya.</p><p style=\"margin-bottom: 16px; font-family: Helvetica, Helvetica-FF, Arial, Tahoma, sans-serif; font-size: 16px;\">Meski begitu, Mantan Wali Kota Solo ini meminta proses pencairan pinjaman dari LPDB ke koperasi dan selanjutnya dipermudah. Selain itu, dirinya juga mengingatkan kepada masing-masing koperasi tidak memberikan bunga tinggi kepada pelaku UMKM.</p><p style=\"margin-bottom: 16px; font-family: Helvetica, Helvetica-FF, Arial, Tahoma, sans-serif; font-size: 16px;\">Tidak hanya itu, Jokowi juga meminta Menteri Koperasi dan UKM Teten Masduki mempercepat lagi program relaksasi dan restrukturisasi bagi pelaku koperasi dan UMKM agar Indonesia terhindar dari perlambatan ekonomi.</p><p style=\"margin-bottom: 16px; font-family: Helvetica, Helvetica-FF, Arial, Tahoma, sans-serif; font-size: 16px;\">\"Ini yang kita inginkan jadi semuanya, koperasinya bergerak, UMKM nya juga sebentar lagi di awal Agustus ini akan kita berikan kepada 12 juta UMKM kita namakan bantuan modal kerja produktif pada 12 juta UMKM yang kita harapkan juga akan mengungkit ekonomi kita,\" ungkap Jokowi.</p><p style=\"margin-bottom: 16px; font-family: Helvetica, Helvetica-FF, Arial, Tahoma, sans-serif; font-size: 16px;\">Source : detik com</p>', '2020-07-23', 'c43cc2d627de0f5ddbd05426b42adc96.jpeg');
-
---
--- Indexes for dumped tables
---
-
---
--- Indeks untuk tabel `berita`
---
-ALTER TABLE `berita`
-  ADD PRIMARY KEY (`id_berita`);
-
---
--- AUTO_INCREMENT untuk tabel yang dibuang
---
-
---
--- AUTO_INCREMENT untuk tabel `berita`
---
-ALTER TABLE `berita`
-  MODIFY `id_berita` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
